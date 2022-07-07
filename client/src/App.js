367204afde9794baf8f08682a7bc5a3e5fafe2db
alterkate <barsukova.ekat@gmail.com>
2022-07-07 17:33:26 +0300

    base components of the game page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Game from './components/Game/Game';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/start" element={<div>Регистрация/Вход/Старт страницы</div>} />
-        <Route path="/game" element={<h1>Welcome to Garbage Game!</h1>} />
+        <Route path="/game" element={<Game />} />
         <Route path="/rules" element={<div>Правила игры</div>} />
         <Route path="/info" element={<div>Правила сортировки</div>} />
         <Route path="*" element={<div>404</div>} />
