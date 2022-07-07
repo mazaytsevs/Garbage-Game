171076e363af9b4079ac0ad5fe146ec09f431763
banu <gula2798@mail.ru>
2022-07-07 16:36:21 +0300

    Auth& routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Nav/Navbar';
+import PrivateRoute from './components/PrivateRouter/PrivateRouter';
+import Registration from './components/Forms/Reg/Registration';
+import Login from './components/Forms/Login/Login';
+import Logout from './components/Forms/Logout/Logout';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/start" element={<div>Регистрация/Вход/Старт страницы</div>} />
-        <Route path="/game" element={<h1>Welcome to Garbage Game!</h1>} />
-        <Route path="/rules" element={<div>Правила игры</div>} />
-        <Route path="/info" element={<div>Правила сортировки</div>} />
-        <Route path="*" element={<div>404</div>} />
+        <Route path="/" element={<Navbar />} />
+        <Route path="/start" element={<PrivateRoute>Start</PrivateRoute>} />
+        <Route path="/game" element={<PrivateRoute>Game</PrivateRoute>} />
+        <Route path="/rules" element={<PrivateRoute>Rules</PrivateRoute>} />
+        <Route path="/info" element={<PrivateRoute>Info</PrivateRoute>} />
+        <Route path="/auth/logout" element={<PrivateRoute><Logout /></PrivateRoute>} />
+        <Route path="/auth/reg" element={<Registration />} />
+        <Route path="/auth/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ client/src/redux/types/types.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+export const DISABLE_LOADER = 'DISABLE_LOADER';
+export const ENABLE_LOADER = 'ACTIVATE_LOADER';
+export const SET_USER = 'SET_USER';
+export const DELETE_USER = 'DELETE_USER';
