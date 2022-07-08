46f730ad44388f5ebaaa023e3cacf568022b7f8f
Alex <alex95po@gmail.com>
2022-07-08 17:59:02 +0300

    gamenav working version

--- client/src/components/GameBins/GameBins.js
+++ client/src/components/GameBins/GameBins.js
@@ -1,6 +1,6 @@
-import GameBinsDragAndDrop from '../GameBinsDragAndDrop/GameBinsDragAndDrop';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import GameBinsDragAndDrop from '../GameBinsDragAndDrop/GameBinsDragAndDrop';
 import { generateTrashThunk } from '../../redux/actions/actions';
 import './gameBins.css';
 
@@ -17,7 +17,7 @@ function GameBins() {
 
       <div><GameBinsDragAndDrop /></div>
       <div className="gameBins">
-        {trashBins.map((el) => <img alt="" src={el.trash_can_img_src} />)}
+        {trashBins?.map((el) => <img alt="" src={el.trash_can_img_src} />)}
       </div>
     </div>
   );
--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import loaderReducer from './loaderReducer';
 import userReducer from './userReducer';
 import infoReducer from './infoReducer';
 import trashGeneratereducer from './trashGeneratereducer';
+import progressReducer from './progressReducer';
 
 const rootReducer = combineReducers({
   info: infoReducer,
   user: userReducer,
   loader: loaderReducer,
   trashGenerate: trashGeneratereducer,
+  progress: progressReducer,
 });
 
 export default rootReducer;
