e966479a95529ccc87b2e087bbc58de351eade62
MariaZobnina <wzobnina.maria@gmail.com>
2022-07-08 19:28:39 +0300

    Bomzh shows

--- client/src/components/GameBomzh/GameBomzh.js
+++ client/src/components/GameBomzh/GameBomzh.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './gameBomzh.css';
 
 function GameBomzh() {
+  const trashBinsFromDB = useSelector((state) => state.trashGenerate?.trashCans);
+  // eslint-disable-next-line no-unsafe-optional-chaining
   return (
     <div className="GameBomzh">
-      <div className="bomzh">
-        <img
-          className="man"
-          src="man/image19.png"
-          alt="man"
-          width="100"
-        />
-      </div>
+      {trashBinsFromDB
+            && (
+            <div className="bomzh" id={(trashBinsFromDB[trashBinsFromDB.length - 1]).id}>
+              <img
+                className="man"
+                src="man/image19.png"
+                alt="man"
+                width="500"
+              />
+            </div>
+            )}
     </div>
   );
 }
