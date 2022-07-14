30df293d4b1f07ef144ff03c24ff3e73fe0ab80a
MariaZobnina <wzobnina.maria@gmail.com>
2022-07-14 17:37:30 +0300

    button added endgame

--- client/src/components/EndGame/EndScreen.js
+++ client/src/components/EndGame/EndScreen.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import classes from './EndGame.module.css';
 
 function EndScreen() {
+  const navigate = useNavigate();
   return (
-    <div className={classes.endscreen} />
+    <div className={classes.endscreen}>
+      <div className="logout">
+        <input
+          type="image"
+          className="logoutButton"
+          alt="exit"
+          src="/gameNavPic/logout.png"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate('/');
+          }}
+        />
+      </div>
+    </div>
   );
 }
 
