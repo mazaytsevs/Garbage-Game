732d4b80fb90595bf52a502fd49c41428b003191
banu <gula2798@mail.ru>
2022-07-14 14:19:32 +0300

    for heroku

--- server/db/database.js
+++ server/db/database.js
@@ -9,17 +9,22 @@ module.exports = {
     dialect: 'postgresql',
   },
   test: {
-    username: 'admin',
-    password: '123',
-    database: 'socker_site',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_URL,
     dialect: 'postgresql',
   },
   production: {
-    username: 'admin',
-    password: '123',
-    database: 'socker_site',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_URL,
     dialect: 'postgresql',
+    dialectOptions: {
+
+      ssl: { rejectUnauthorized: false },
+
+    },
   },
 };
